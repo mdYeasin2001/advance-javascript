87a74b59602525800c8ea168f404b6d71da5635b
Yeasin <yeasin30112001@gmail.com>
2021-02-02 09:26:59 -0600

    some note added

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -17,9 +17,13 @@ console.log(result);
 const myArray = [3, 5, 9];
 console.log(myArray[5]);
 
-const setValue = undefined;
+const setValue = undefined; // undefined value should not be set by developer;
 console.log(setValue);
 
 ===================== null ==================
-let name = null;
+let name = null; // if needed developer can set a null value;
 console.log(name);
+
+// undefined: when we try to get or access a value that not set yet than we get undefined error. there are some example above 
+
+// null: null means empty, means there is no value;
