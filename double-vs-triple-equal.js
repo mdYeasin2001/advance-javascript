fdb4b65e6201435bf0017d86766c43a3052cf444
Yeasin <yeasin30112001@gmail.com>
2021-02-02 09:44:53 -0600

    double vs triple equal cleared

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,22 @@
+const num1 = 0;
+// const num2 = 3;
+// const num2 = '2';
+// const num2 = true;
+const num2 = false;
+if(num1 == num2){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
+
+if(num1 === num2){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
+
+// double equal: when we use double-equal(==) for check condition than it convert the type of value or it dose not check of the type of value . It just check the provided value equal or not.
+
+// strick equal or triple equal: when we use triple-equal(===) for check condition it check the provided value equal or not and it's type also. If the value and the type is same than the condition will be true;
