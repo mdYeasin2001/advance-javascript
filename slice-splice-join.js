abe71cb9432cd1b45ecdc3b1f03ec4a78179c13f
Yeasin <yeasin30112001@gmail.com>
2021-02-03 18:20:34 -0600

    learn slice, splice, join method

---
+++ slice-splice-join.js
@@ -0,0 +1,6 @@
+const numbers = [3, 4, 5, 6, 7, 8, 9];
+const part = numbers.slice(3, 5);
+const part2 = numbers.splice(1, 3);
+const joined = numbers.join('.');
+console.log(joined);
+console.log(numbers);
